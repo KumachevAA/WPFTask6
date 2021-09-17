494709d0c51c7ffdc7ffd24b4c9c5b63db1ae940
Anton <Anton@anton.anton>
2021-09-17 19:26:59 +0300

    task completed

--- WPFTask6.1/MainWindow.xaml.cs
+++ WPFTask6.1/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -35,6 +36,24 @@ namespace WPFTask6._1
             {
                 string path = dialog.SelectedPath;
                 folderBox.Text = path;
+            }
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            if (!Directory.Exists(folderBox.Text))
+                return;
+
+            DirectoryInfo search = new DirectoryInfo(folderBox.Text);
+            string pattern = searchBox.Text;
+            SearchOption recursive = recursiveSearch.IsChecked == true ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly;
+
+            FileInfo[] files = search.GetFiles(pattern, recursive);
+            results.Text = string.Empty;
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                results.Text += files[i].FullName + Environment.NewLine;
             }
         }
     }
