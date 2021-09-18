b7927dcbd86947767d7c388d4376c3f5388cf298
Anton <Anton@anton.anton>
2021-09-18 22:03:43 +0300

    directory navigation

--- WPFTask6.3/MainWindow.xaml.cs
+++ WPFTask6.3/MainWindow.xaml.cs
@@ -12,7 +12,6 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WPFTask6._3
 {
@@ -21,11 +20,72 @@ namespace WPFTask6._3
     /// </summary>
     public partial class MainWindow : Window
     {
-        DirectoryInfo currentDirectory = new DirectoryInfo("C:/");
+        DirectoryInfo currentDirectory;
+        FileSystemInfo[] entries;
+
+        void UpdateFileSystemEntries(string path)
+        {
+            if (Directory.Exists(path))
+            {
+                DirectoryInfo temp = currentDirectory;
+
+                try
+                {
+                    currentDirectory = new DirectoryInfo(path);
+                    entries = currentDirectory.EnumerateFileSystemInfos().ToArray();
+
+                    FileExplorer.Items.Clear();
+
+                    FileExplorer.Items.Add("..");
+                    for (int i = 0; i < entries.Length; i++)
+                    {
+                        FileExplorer.Items.Add(entries[i].Name);
+                    }
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show($"Ошибка при чтении содержимого по пути {currentDirectory.FullName}");
+                    currentDirectory = temp;
+                }
+            }
+
+            PathInput.Text = currentDirectory.FullName;
+        }
 
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            UpdateFileSystemEntries("C:/");
+        }
+
+        private void ListViewItem_Preview(object sender, MouseButtonEventArgs e)
+        {
+            int selected = FileExplorer.SelectedIndex;
+
+            if (selected == 0)
+            {
+                UpdateFileSystemEntries(currentDirectory.Parent.FullName);
+            }
+            if (selected > 0)
+            {
+                FileSystemInfo entry = entries[selected - 1];
+
+                if (entry.Exists)
+                {
+                    if (entry is DirectoryInfo dir)
+                    {
+                        UpdateFileSystemEntries(dir.FullName);
+                    }
+                    else if (entry is FileInfo file)
+                    {
+                        MessageBox.Show("File selected");
+                    }
+                }
+            }
         }
     }
 }
